ac28d7f7eabf9d0f6176c1f1133c3f589f86e01a
jasmineranay <102012764+jasmineranay@users.noreply.github.com>
2022-06-12 13:26:55 -0700

    add routes

---
+++ Develop/routes/htmlRoutes/index.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const router = require('express').Router()
+
+// HTML Routes
+// GET Route for notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+// Export module
+module.exports = router;
